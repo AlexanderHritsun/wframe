7daab1b310dc883783c7d4edb9d5509cffb185c7
Alexander Hritsun <47444468+AlexanderHritsun@users.noreply.github.com>
2021-03-30 12:30:09 +0300

    Integration of the selenium stand alone service

--- wdio.conf.js
+++ wdio.conf.js
@@ -46,17 +46,17 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [
-            {
-                maxInstances: 5,
-                browserName: 'chrome',
-                acceptInsecureCerts: true
-            },
-            {
-                maxInstances: 5,
-                browserName: 'firefox',
-                acceptInsecureCerts: true
-            }
-        ],
+        {
+            browserName: 'chrome',
+            port: 5555
+        }, {
+            browserName: 'firefox',
+            port: 5555
+        }, {
+            browserName: 'MicrosoftEdge',
+            port: 5555
+        }
+    ],
     //
     // ===================
     // Test Configurations
@@ -104,8 +104,16 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    
+    services: [
+        ['selenium-standalone', {
+            logPath: './temp',
+            args: {
+                version: "3.141.59",
+                seleniumArgs: ['-host', '127.0.0.1','-port', '5555']
+            },
+        }]
+    ],
+
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
